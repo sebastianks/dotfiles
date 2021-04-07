ecfc18aeb2058077253150d5d67016e1f77caeb0
Sebastian K. Sorensen <sks1993@gmail.com>
2021-04-07 08:03:04 +0200

    Fix fzf keybindings path

--- bashrc.d/plugins.sh
+++ bashrc.d/plugins.sh
@@ -10,5 +10,5 @@ export FZF_DEFAULT_COMMAND="fd --type f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d . $HOME"
 
-source /usr/share/doc/fzf/examples/key-bindings.bash
+source /usr/share/fzf/key-bindings.bash
 
